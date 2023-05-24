0489742ee12ee9cd47fba8c2b30527bbe5f3ae53
Marcelohxt <marcelo_hxt@hotmail.com>
2023-05-24 13:49:00 -0300

    REMOVIDO CONTATO

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 
-from recipes.views import home, contato, sobre
-
+from recipes.views import home
 
 urlpatterns = [
     path ('', home),
-    path('Sobre/', sobre),
-    path('contato/', contato),
 ]
